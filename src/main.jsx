548012f85f9a8d190c019f13bf4494e7e472809e
TeliStaffs <97461050+User083@users.noreply.github.com>
2023-06-26 18:35:38 +0100

    Apollo/GraphQL setup
    - Basic Apollo client setup with countries api

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.scss'
+import { ApolloProvider } from '@apollo/client'
+import { client } from './apolloClient.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <ApolloProvider client={client}>
     <App />
+    </ApolloProvider>
   </React.StrictMode>,
 )
